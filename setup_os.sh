73bb6f3346278e968ee29f8c28b49181dcb6a7de
ADIJS-CMD <gilaradifirmansyah@gmail.com>
2025-12-10 17:18:45 +0700

    Delete setup_os.sh

--- setup_os.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo apt-get update -y
-sudo apt-get install -y ssh python3 wget curl
-echo "codespace:admin123" | sudo chpasswd
-sudo sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/' /etc/ssh/sshd_config
-sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
-sudo service ssh start
-echo "UBUNTU SYSTEM READY."
