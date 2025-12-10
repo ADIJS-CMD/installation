a549abfc18d61321ee5044f16e4c8c3fc0ad24d7
ADIJS-CMD <gilaradifirmansyah@gmail.com>
2025-12-10 17:20:34 +0700

    Create setup_domain.sh

---
+++ setup_domain.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl -SsL https://playit-cloud.github.io/ppa/key.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/playit.gpg >/dev/null
+echo "deb [signed-by=/etc/apt/trusted.gpg.d/playit.gpg] https://playit-cloud.github.io/ppa/data ./" | sudo tee /etc/apt/sources.list.d/playit.list
+sudo apt-get update
+sudo apt-get install -y playit
+playit
